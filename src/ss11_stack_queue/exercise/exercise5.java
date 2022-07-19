c819fc3de22c99341b789386485ebc56090d6ecd
buichibao <cunglazy0404@gmail.com>
2022-07-19 08:33:41 +0700

    update

--- src/ss11_stack_queue/exercise/exercise5.java
+++ src/ss11_stack_queue/exercise/exercise5.java
@@ -1,15 +1,27 @@
 package ss11_stack_queue.exercise;
 
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class exercise5 {
     public static void main(String[] args) {
+        Map<String,Integer> stringMap = new TreeMap<>();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập chuỗi cần kiểm tra");
         String str = scanner.nextLine();
         String [] arr = str.toLowerCase().split(" ");
         System.out.println(Arrays.toString(arr));
 
+        for (String Array:arr) {
+            if(!stringMap.containsKey(Array)){
+                stringMap.put(Array,1);
+            }else {
+                int count = stringMap.get(Array);// lấy ra value
+                stringMap.replace(Array,count+1);
+            }
+        }
+        System.out.println(stringMap);
     }
 }
